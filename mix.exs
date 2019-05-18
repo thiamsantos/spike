6c5d38859f5da2291745f45ec839d0d7c31390bc
Thiago Santos <thia.mdossantos@gmail.com>
2019-05-18 17:33:05 -0300

    Configure ex_doc

--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule Spike.MixProject do
     [
       app: :spike,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: "Spike",
+      package: package(),
+      docs: docs(),
+      name: "Spike",
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -25,11 +29,28 @@ defmodule Spike.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["Thiago Santos"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/thiamsantos/spike"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Spike",
+      source_url: "https://github.com/thiamsantos/spike",
+      extras: ["README.md"]
+    ]
+  end
+
   defp deps do
     [
       {:amqp, "~> 1.1"},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.11", only: :test, runtime: false}
+      {:excoveralls, "~> 0.11", only: :test, runtime: false},
+      {:ex_doc, "~> 0.20", only: :dev, runtime: false}
     ]
   end
 end
