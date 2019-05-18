81bba7ae460af7d4fbc55286875aa8c2858c85ac
Thiago Santos <thia.mdossantos@gmail.com>
2019-05-18 17:33:52 -0300

    Format

--- test/spike_test.exs
+++ test/spike_test.exs
@@ -43,7 +43,7 @@ defmodule SpikeTest do
 
       assert Version.match?(
                app_version,
-             readme_versions
+               readme_versions
              ),
              """
              Install version constraint in README.md does not match to current app version.
